969bc7e9454e07f1417968acb8bd62c33d95cf4f
Caspar van Vuure <70904913+CSPR07@users.noreply.github.com>
2022-04-08 12:16:53 +0200

    Update index.php

--- index.php
+++ index.php
@@ -24,39 +24,45 @@
         <article style='margin-top:0px'>
             <h2>Intro website</h2>
 
-            <p>Dit is een website om mijn html/css/php skills te laten zien in praktijk. Je kan op deze website een datum invullen en vervolgens krijg je auto's die in die periode beschikbaar zijn. dit kan je vinden onder het kopje 'bestellen'</p>
+            <p>Dit is een website om mijn html, css, php en sql skills te laten zien in praktijk. Je kan op deze website een datum invullen en vervolgens krijg je auto's die in die periode beschikbaar zijn. dit kan je vinden hier onder</p>
         </article>
         
         <article>
+        
         <form id="" method="POST" action="index.php" >
-            <input type="search" name="date" value="" placeholder="JJJJ-MM-DD" id="zoeken">
+            <input type="date" name="datehuren" value="datehuren" id="zoeken">
             <input type="submit" value="zoek">
         </form>
         <br>
         <?php
-        include_once 'conn.php';
+        $servername = "localhost";
+        $username = "id18739086_root";
+        $password = "GxkC1N4Mx!Hh&--^";
+        $database = "id18739086_tools4ever";
         $conn = new mysqli($servername, $username, $password, $database);
         
-            if(isset($_POST["date"]) && !empty($_POST["date"])){
-                $_SESSION["date"] = $_POST["date"];
+        
+            if(isset($_POST["datehuren"]) && !empty($_POST["datehuren"])){
+                $_SESSION["datehuren"] = $_POST["datehuren"];
             }
         
-        if(isset($_SESSION["date"])){
+        if(isset($_SESSION["datehuren"])){
         
-        $sql = "SELECT * FROM auto WHERE datum = '" . $_SESSION["date"] . "'"; 
+        $sql = "SELECT auto.naam Naamauto, autohuren.begindatum Begindate, autohuren.einddatum Einddate FROM auto INNER JOIN autohuren ON auto.id = autohuren.id WHERE Begindate < '" . $_SESSION['datehuren'] . "' AND Einddate > '" . $_SESSION['datehuren'] . "'";
         $result = $conn->query($sql);
-        while($row = $result->fetch_array()){
-        echo $row['naam'];
+        while($row = $result ->fetch_array()){
         echo "<br>";
-        echo $row['datum'];
+        echo $row['Naamauto'];
         echo "<br>";
-        echo "€"; echo $row['prijs'];
+        echo $row['Begindate'];
         echo "<br>";
-        echo $row['brandstof'];
+        echo $row['Einddate'];
         echo "<br>";
+        echo "---------------------------------------";
         }
         $conn-> close();
         }
+
         ?>
         </article>
     </main>
