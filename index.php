8208f862c7f9959348718cae85aa1eb524a7c093
Caspar van Vuure <70904913+CSPR07@users.noreply.github.com>
2022-04-12 10:34:42 +0200

    Update index.php

--- index.php
+++ index.php
@@ -24,40 +24,47 @@
         <article style='margin-top:0px'>
             <h2>Intro website</h2>
 
-            <p>Dit is een website om mijn html/css/php skills te laten zien in praktijk. Je kan op deze website een datum invullen en vervolgens krijg je auto's die in die periode beschikbaar zijn. dit kan je vinden onder het kopje 'bestellen'</p>
+            <p>Dit is een website om mijn html, css, php en sql skills te laten zien in praktijk. Je kan op deze website een datum invullen en vervolgens krijg je auto's die in die periode beschikbaar zijn. dit kan je vinden hier onder</p>
         </article>
-        
+
         <article>
-        <form id="" method="POST" action="index.php" >
-            <input type="search" name="date" value="" placeholder="JJJJ-MM-DD" id="zoeken">
-            <input type="submit" value="zoek">
-        </form>
-        <br>
-        <?php
-        include_once 'conn.php';
-        $conn = new mysqli($servername, $username, $password, $database);
-        
-            if(isset($_POST["date"]) && !empty($_POST["date"])){
-                $_SESSION["date"] = $_POST["date"];
+
+            <form id="" method="POST" action="index.php" >
+                <input type="date" name="datehuren" value="datehuren" id="zoeken">
+                <input type="submit" value="zoek">
+            </form>
+            <br>
+            <?php
+            include_once 'conn.php';
+            $conn = new mysqli($servername, $username, $password, $database, $port);
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
             }
-        
-        if(isset($_SESSION["date"])){
-        
-        $sql = "SELECT * FROM auto WHERE datum = '" . $_SESSION["date"] . "'"; 
-        $result = $conn->query($sql);
-        while($row = $result->fetch_array()){
-        echo $row['naam'];
-        echo "<br>";
-        echo $row['datum'];
-        echo "<br>";
-        echo "€"; echo $row['prijs'];
-        echo "<br>";
-        echo $row['brandstof'];
-        echo "<br>";
-        }
-        $conn-> close();
-        }
-        ?>
+            ?>
+            <?php
+            if(isset($_POST["datehuren"]) && !empty($_POST["datehuren"])){
+                $_SESSION["datehuren"] = $_POST["datehuren"];
+            }
+
+            if(isset($_SESSION["datehuren"])){
+
+                $sql = "SELECT auto.naam, autohuren.begindatum, autohuren.einddatum 
+                        FROM autohuren 
+                        INNER JOIN auto ON autohuren.id = auto.id 
+                        WHERE autohuren.begindatum < '" . $_SESSION['datehuren'] . "' 
+                        AND autohuren.einddatum > '" . $_SESSION['datehuren'] . "';";
+                $result = $conn->query($sql);
+                while($row = $result ->fetch_array()){
+                    echo "<br> {$row['naam']}".
+                    "<br> {$row['begindatum']}". 
+                    "<br> {$row['einddatum']}".
+                    "<br>".
+                    "---------------------------------------";
+                }
+                $conn-> close();
+            }
+
+            ?>
         </article>
     </main>
 
@@ -68,7 +75,3 @@
 
 </body>
 </html>
-<?php
-
-
-?>
